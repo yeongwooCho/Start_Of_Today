41618a7dd4c067a470d20f4a46b9b50fbd65197e
yeongwoo-Cho <jyy0223@naver.com>
2020-09-05 15:57:14 +0900

    update board error

--- first_project/board/views.py
+++ first_project/board/views.py
@@ -4,9 +4,9 @@ from .scraping import scraping
 
 
 def index(request):
-    smarts = Board.objects.filter(num='1')
-    notices = Board.objects.filter(num='2')
-    emps = Board.objects.filter(num='3')
-    pusanits = Board.objects.filter(num='4')
+    smarts = Board.objects.filter(num='1').order_by('-rate')
+    notices = Board.objects.filter(num='2').order_by('-rate')
+    emps = Board.objects.filter(num='3').order_by('-rate')
+    pusanits = Board.objects.filter(num='4').order_by('-rate')
 
     return render(request, 'index.html', {'scp_notices': notices, 'scp_pusanits': pusanits, 'scp_emps': emps, 'scp_smarts': smarts})
