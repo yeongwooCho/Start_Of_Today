cfb97d432bb15dce77f04421090fc212bb739b05
yeongwoo-Cho <jyy0223@naver.com>
2020-09-10 16:28:03 +0900

    Add update button

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
     path('polls/', include('polls.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
 ]
--- first_project/home/views.py
+++ first_project/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .get_wise import get_Wise
+
+# from resultregistration.scraping import scraping
+from board.scraping import scraping as bs
+from polls.scraping import scraping as ps
 # Create your views here.
 
 
@@ -7,3 +11,10 @@ def home(request):
     wise_saying = get_Wise()
 
     return render(request, "home.html", {'wise_saying': wise_saying})
+
+
+def updates(request):
+    bs()
+    ps()
+
+    return render(request, "updates.html")
